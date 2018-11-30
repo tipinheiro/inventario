f226389a49dd55faf01742cfb14e8c08bbed5d13
kerneldark <kerneldark@gmail.com>
2018-11-30 15:29:51 -0300

    t

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return '<center><h1>Seja bem vindo ao laravel</h1></center>';
+    return view('auth.login');
+});
+
+Route::get('/equipamentos', 'EquipamentoController@lista');
+
+Route::get('/admin/cadastro', 'EquipamentoController@cadastroEquipamento');
+
+Route::get('/equipamentos/mostra/{id}', 'EquipamentoController@mostra');
+
+Route::post('/equipamento/salvar', 'EquipamentoController@salvar');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
