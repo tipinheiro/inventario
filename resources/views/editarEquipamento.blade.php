bc41632a3c7dd827bf491c26a871b7ca17b9fb54
tipinheiro <tipinheiro@ufma.br>
2018-12-04 18:27:20 -0300

    Correção 2
    Co-Authored-By: tipinheiro <tipinheiro@ufma.br>

--- resources/views/editarEquipamento.blade.php
+++ resources/views/editarEquipamento.blade.php
@@ -147,7 +147,7 @@
                   <td>Mike Doe</td>
                   <td>11-7-2014</td>
                   <td><span class="label label-danger">Denied</span></td>
-                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback </td>
                 </tr>
               </table>
             </div>
