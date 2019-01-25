47e2c414d7414871610581dd6c95b2ee0568b87a
kerneldark <kerneldark@gmail.com>
2019-01-25 16:15:48 -0300

    menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace inventario\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+use inventario\Equipamento;
+use inventario\acessorios;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,10 +15,28 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+
+    public function boot(Dispatcher $events)
     {
-        //
-        //Schema::defaultStringLength(191);
+        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+            $event->menu->add('MAIN NAVIGATION');
+            $event->menu->add([
+                'text'        => 'Users',
+                'url'         => 'admin/users',
+                'icon'        => 'users',
+                'label'       =>  Equipamento::count(),
+                'label_color' => 'success',
+            ]);
+
+            $event->menu->add('MAIN NAVIGATION 2');
+            $event->menu->add([
+                'text'        => 'Users',
+                'url'         => 'admin/users',
+                'icon'        => 'users',
+                'label'       =>  acessorios::count(),
+                'label_color' => 'success',
+            ]);
+        });
     }
 
     /**
